463c9caab3a9d824ec453dc728078f9a61e3a1b3
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-08-24 11:24:20 +0200

    Create sidebar.py

---
+++ lib/sidebar.py
@@ -0,0 +1,31 @@
+import os
+import webbrowser
+
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+
+def show_sidebar():    
+    with st.sidebar:
+        with st.expander("About MusicTools"):
+            with open("res/about.txt") as file:
+                st.write(file.read())
+        
+        columns = st.columns(2)
+        
+        columns[0].button(
+                "View on GitHub",
+                on_click=lambda: webbrowser.open("https://github.com/davi4046/music_tools_app")
+        )
+        
+        columns[1].button(
+                "Open License", 
+                on_click=lambda: webbrowser.open(os.path.abspath("license.txt"))
+        )
+            
+        columns = st.columns(2)
+        
+        columns[0].button(
+                "Buy Me a Coffee ☕",
+                on_click=lambda: webbrowser.open("https://www.buymeacoffee.com/davidrudpedersen")
+        )
